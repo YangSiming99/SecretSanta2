4c6b18bb5f16f14b9c99b4d82a05122e07871256
Siming <yangsiming1999@gmail.com>
2020-09-08 11:51:42 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+  const password = Array.from(Array(count).keys()).map(i => generatePassword(12, false))
+  res.json(password);
+  console.log(`sent ${count} passwords`)
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'))
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`password generator listening on port ${port}`);
